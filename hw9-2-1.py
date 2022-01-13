abc445c5731c0a59112ef3b6c9bc6fd700e067ef
p22strautz <p22strautz@fairfieldprep.org>
2022-01-13 10:19:37 -0500

    finish hw 9-2

---
+++ hw9-2-1.py
@@ -0,0 +1,9 @@
+# Author: SCT (AMDG) 1/13/22
+
+last_initials = ["B", "D", "H", "E", "G", "G", "H", "R", "M", "L", "I", "I", "N", "N", "O", "P", "P", "X", "T", "S", "S", "P"]
+
+rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin" "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
+
+for row in rows:
+    for i, v in enumerate(rows):
+        
